80fe9529facbe888fb11c0431c98d293888cc0b7
Harrison Malone <harrison.malone2@gmail.com>
2021-01-19 12:31:27 +1100

    added validation and model methods"

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,12 @@
 class Subscription < ApplicationRecord
+  validates :name, presence: true
+  validates :price_per_month, presence: true
+  validates :billing_period, presence: true
+
+  def capitalize_name
+    capitalized_words = name.split(" ").map do |word|
+      word.capitalize
+    end
+    self.name = capitalized_words.join(" ")
+  end
 end
