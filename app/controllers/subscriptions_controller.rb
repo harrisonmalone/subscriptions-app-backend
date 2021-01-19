92e9ab7d256cf0ca0710b0a7b796555c41ea586c
Harrison Malone <harrison.malone2@gmail.com>
2021-01-19 12:31:04 +1100

    added before_action for setting a subscription, added update and destroy actions

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,4 +1,6 @@
 class SubscriptionsController < ApplicationController
+  before_action :set_subscription, only: [:show, :destroy, :update]
+
   def index 
     # send all of the subscriptions back
     subscriptions = Subscription.all
@@ -7,13 +9,13 @@ class SubscriptionsController < ApplicationController
 
   def show 
     # send one subscription back
-    subscription = Subscription.find(params[:id])
-    render json: subscription
+    render json:  @subscription
   end
 
   def create 
     # create a new subscription
     subscription = Subscription.new(subscriptions_params)
+    subscription.capitalize_name
     if subscription.save
       render status: :created 
     else
@@ -21,11 +23,27 @@ class SubscriptionsController < ApplicationController
     end
   end
 
-  # strong params
+  def update 
+    # updating one subscription
+    if @subscription.update(subscriptions_params)
+      render status: :no_content
+    else
+      render status: :bad_request
+    end
+  end
+
+  def destroy 
+    @subscription.destroy
+  end
 
   private 
 
+  def set_subscription 
+    @subscription = Subscription.find(params[:id])
+  end
+  
   def subscriptions_params 
+    # strong params
     params.require(:subscription).permit(:name, :price_per_month, :billing_period)
   end
 end
