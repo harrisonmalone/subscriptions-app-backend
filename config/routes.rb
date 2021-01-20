a674ce2f9335f34b333f11fba428eaf93b74712b
Harrison Malone <harrison.malone2@gmail.com>
2021-01-20 11:54:32 +1100

    added routes for authentication logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  post "/login", to: "user_token#create"
+  post "/sign-up", to: "users#create"
+  get "/status", to: "status#index"
+  get "/status/user", to: "status#user"
   resources :subscriptions
 end
